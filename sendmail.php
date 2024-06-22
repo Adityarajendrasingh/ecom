557c6ef028852d03ff882ea882f0fc76c57d54dd
Aditya Singh <83662394+Adityarajendrasingh@users.noreply.github.com>
2024-06-22 18:12:11 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -53,7 +53,7 @@ if(isset($_POST['submit-customer-form'])) {
         
         $mail->Host       = 'smtp.gmail.com';
         $mail->Username   = 'webemailfornewsite@gmail.com';
-        $mail->Password   = 'kymmdcdbajuonfcq';
+        $mail->Password   = '';
 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
